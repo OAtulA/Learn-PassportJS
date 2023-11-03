f6431feb11a60a558966e9cbe928e2da240961bb
OAtulA <oraonatula@gmail.com>
2023-11-03 19:53:15 +0530

    All routes working fine. Corrected the condition to check user logged in.

--- Request tests/post-user2.js
+++ Request tests/post-user2.js
@@ -1,73 +1,60 @@
-const fs = require('fs')
-let format_user=[];
-fs.readFile('Bunch-of-users.txt', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  users = data;
-  newUsers = users.split('\n')
-  console.log('newUsers= ', newUsers)
-  format_user = newUsers.map(user => {
-    user2 = user.split(' ');
-    new_user = { name: user2[0], password: user2[1] };
-    return new_user;
-  });
+const fs = require('fs').promises;
+const { resolve } = require('path');
+let format_user = [];
 
-  console.log('format_user=',format_user)
-  console.log(format_user.length)
-  for(let i=0; i< format_user.length; i++ )
-  { let user = format_user[i];
-  fetch('http://localhost:8002/signup', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      name: user.name,
-      password: user.password
-    })
-  })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error('Error creating user');
-      }
-    })
-    .then(data => {
-      console.log('New user created:', data);
-    })
-    .catch(error => {
-      console.error('Error:', error);
+async function getUsers() {
+ try {
+    const data = await fs.readFile('Bunch-of-users.txt', 'utf8');
+    users = data;
+    newUsers = users.split('\n')
+    console.log('newUsers= ', newUsers)
+    format_user = newUsers.map(user => {
+      let user2 = user.split(' ');
+      let new_user = { name: user2[0], password: user2[1] };
+      return new_user;
     });
+ } catch (err) {
+    console.error(err);
+ }
 }
-});
-// console.log(format_user.length)
-// format_user.forEach(user => 
-for(let i=0; i< format_user.length; i++ )
-  { let user = format_user[i];
-  fetch('http://localhost:8002/users', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      name: user.name,
-      password: user.password
+
+function postUsers() {
+ console.log('format_user=', format_user)
+ console.log('no. of user signups: ',format_user.length)
+
+ const fetchRequests = format_user.map(user => {
+    return fetch('http://localhost:8002/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: user.name,
+        password: user.password
+      })
     })
-  })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error('Error creating user');
-      }
-    })
-    .then(data => {
-      console.log('New user created:', data);
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Error creating user');
+        }
+      })
+      .then(data => {
+        console.log('New user created:', data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+ });
+
+ Promise.all(fetchRequests).then(() => console.log('All users have been signed up.'));
 }
+
+let signups = async ()=>{
+ await getUsers()
+ //Login check
+ postUsers();
+}
+
+signups()
