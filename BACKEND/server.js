3693188a63b40db81779b376a50959da86468764
Atul <oraonatula@gmail.com>
2024-02-07 16:08:22 +0530

    Learnt how to appendChild and remove it

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -2,11 +2,15 @@ const express = require("express");
 const app = express();
 const userRoutes = require("./routes/usersRoutes");
 const pagesRoutes = require('./routes/pagesRoutes')
+const path = require("path")
 
 app.use(express.json());
-app.use("/", userRoutes);
-app.use('/', pagesRoutes)
+// app.use("/", userRoutes);
+// app.use('/', pagesRoutes)
 
+app.get('/', (req, res)=>{
+  res.sendFile(path.resolve("views/index.html"));
+})
 
 app.listen(8002, () => {
   console.log("Server started\n Listening on http://localhost:8002");
