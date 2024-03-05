6d55f2291cb3392f495d7b9fcd5f802b46c80ab2
Atul <oraonatula@gmail.com>
2024-03-06 00:08:02 +0530

    getting the cookies and verification done with sameSite:None

--- client/src/pages/protected.jsx
+++ client/src/pages/protected.jsx
@@ -1,22 +1,25 @@
 import React from "react";
-
+import axios from "axios";
 const ProtectedPage = () => {
   const protectedCheck = () => {
-    const options = {
-      method: "POST",
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify({
-        email: "kaduMosai@mail.com",
-        password: "123",
-      }),
+    const requestData = {
+      email: "kaduMosai@mail.com",
+      password: "123",
     };
 
-    fetch("http://localhost:8002/auth/protected", options)
-      .then((response) => response.json())
-      .then((response) => console.log(response))
-      .catch((err) => {
-        alert(err);
-        console.log(err);
+    axios
+      .post("http://localhost:8002/auth/protected", requestData, {
+        headers: {
+          "Content-type": "application/json",
+        },
+        withCredentials: true,
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        alert(error.message);
+        console.error(error);
       });
   };
 
